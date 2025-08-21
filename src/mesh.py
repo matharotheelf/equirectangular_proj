70d8e1206d3209ea194a8577f51abafd163991ba
Tom Lawrence <mrthomasjacoblawrence@hotmail.co.uk>
2025-08-21 21:45:43 +0100

    Write a program to generate an array of triangles from coordinates
    1. coordinates are grouped into triangles which can individually be
       rendered by moderngl to the screen

--- src/mesh.py
+++ src/mesh.py
@@ -1,10 +1,11 @@
 import moderngl
+import numpy as np
 
 class Mesh:
     def __init__(self, program, panel):
         self.colour = panel.colour
         self.ctx = moderngl.get_context()
-        vertices = panel.equi_coordinates
+        self.triangles = self.generate_mesh(panel.equi_coordinates)
 
         self.vbo = self.ctx.buffer(vertices.astype('f4').tobytes())
         self.vao = self.ctx.vertex_array(program, [(self.vbo, '3f 12x 8x', 'in_vertex')])
@@ -12,3 +13,31 @@ class Mesh:
     def render(self):
         self.vao.program['color'] = self.colour
         self.vao.render()
+
+    def generate_mesh(self, coordinate_rows):
+        """
+        Generate mesh from coordinate rows.
+
+        :param coordinate_rows: List of coordinate rows.
+        :return: None
+        """
+
+        triangle_mesh = []
+
+        for row_index, row in enumerate(coordinate_rows[:-1]):
+            next_row = coordinate_rows[row_index + 1]
+
+            for column_index, coordinate in enumerate(row[:-1]):
+                right_coordinate = row[column_index + 1]
+                bottom_coordinate = next_row[column_index]
+
+                vertices = np.asarray([
+                    coordinate,
+                    right_coordinate,
+                    bottom_coordinate
+
+                ], dtype='f4').ravel()
+
+                triangle_mesh.append(vertices)
+
+        return triangle_mesh
